052194c1f646d4dea62d5df054697e2d183ce875
Gregory Row <sqysh@Gregorys-Mac-mini.local>
2025-05-09 10:56:29 -0400

    chore(home-hero):Update home hero images to background images; delete unused home block files

--- app/components/home/HomeHeroCarousel.tsx
+++ app/components/home/HomeHeroCarousel.tsx
@@ -1,6 +1,5 @@
 import useSingleImageCarousel from '@/app/hooks/useSingleImageCarousel'
 import React, { FC } from 'react'
-import Picture from '../common/Picture'
 
 const HomeHeroCarousel: FC<{ images: any; interval: number }> = ({ images, interval = 3000 }) => {
   const currentImage = useSingleImageCarousel(images, interval)
@@ -8,15 +7,13 @@ const HomeHeroCarousel: FC<{ images: any; interval: number }> = ({ images, inter
   return (
     <div className="relative w-full h-full overflow-hidden">
       {images.map((image: { imageUrl: string }, index: number) => (
-        <Picture
+        <div
           key={index}
-          src={image.imageUrl}
-          alt={`Slide ${index + 1}`}
-          className={`absolute w-full h-full object-cover transition-opacity duration-1000 ${
+          style={{ backgroundImage: `url(${image.imageUrl})` }}
+          className={`bg-no-repeat bg-cover origin-bottom w-full h-full absolute transition-opacity duration-1000 ${
             image.imageUrl === currentImage ? 'opacity-100' : 'opacity-0'
           }`}
-          priority={true}
-        />
+        ></div>
       ))}
     </div>
   )
