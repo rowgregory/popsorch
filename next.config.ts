89c0a1e517649d91e23ed0b1a78b814332443318
Gregory Row <rowgregory@gmail.com>
2025-07-10 23:31:52 -0400

    chore(next-config):Add contentsquare

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'c.ba.contentsquare.net'
       }
     ]
   }
