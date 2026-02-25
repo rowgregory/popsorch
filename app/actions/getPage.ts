f16cf296add6235e49329f48b27108135c4c55c8
Gregory Row <rowgregory@gmail.com>
2026-02-25 15:11:48 -0500

    chore(get-page):Remove unstable cache from getPage server action

--- app/actions/getPage.ts
+++ app/actions/getPage.ts
@@ -1,35 +1,25 @@
 import prisma from '@/prisma/client'
-import { unstable_cache } from 'next/cache'
 import { createLog } from '../utils/logHelper'
 
-export const getPage = unstable_cache(
-  async (slug: string) => {
-    try {
-      if (!slug) {
-        throw new Error('Page slug is required')
-      }
-
-      const page = await prisma.page.findUnique({
-        where: { slug }
-      })
-
-      //   if (!page) {
-      //     await createLog('info', 'Page not found', { slug })
-      //     return null
-      //   }
-
-      return page
-    } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : 'Failed to fetch page'
-
-      await createLog('error', 'Failed to fetch page', {
-        error: errorMessage,
-        slug
-      })
-
-      throw new Error(errorMessage)
+export const getPage = async (slug: string) => {
+  try {
+    if (!slug) {
+      throw new Error('Page slug is required')
     }
-  },
-  ['getPage'],
-  { tags: ['Page'] }
-)
+
+    const page = await prisma.page.findUnique({
+      where: { slug }
+    })
+
+    return page
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : 'Failed to fetch page'
+
+    await createLog('error', 'Failed to fetch page', {
+      error: errorMessage,
+      slug
+    })
+
+    throw new Error(errorMessage)
+  }
+}
