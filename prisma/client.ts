cd49dfb8947c58ad124400013a326402d19b5554
Gregory Row <rowgregory@gmail.com>
2026-04-27 22:12:35 -0400

    chore(prisma):Update prisma client

--- prisma/client.ts
+++ prisma/client.ts
@@ -4,6 +4,6 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
